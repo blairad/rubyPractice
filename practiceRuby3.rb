dd2bd4d693e519ace31d6f731f06f3cd38825065
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 15:36:21 +0000

    dare to compare

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,2 +1,8 @@
-problem = false
-print "you can't do that!" unless problem
+# test_1 should be false
+test_1 = 100 != 100
+
+# test_2 = should be false
+test_2 = 666 == 616
+
+# test_3 = should be true
+test_3 = 123 > 122
