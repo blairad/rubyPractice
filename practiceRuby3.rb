4587d42790a9d4f4006d602818c058ec64012c75
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 15:21:25 +0000

    or

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,8 +1,8 @@
-# boolean_1 = 77 < 78 && 77 < 77
-boolean_1 = false
+# boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
 
-# boolean_2 = true && 100 >= 100
-boolean_2 = true
+# boolean_2 = false || -10 > -9
+boolean_2 = false
 
-# boolean_3 = 2**3 == 8 && 3**2 == 9
-boolean_3 = true
+# boolean_3 = false || false
+boolean_3 = false
