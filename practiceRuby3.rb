fdc9079f1d6747ce608cbbf391925465777cc514
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 14:56:37 +0000

    Elsif

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,5 +1,7 @@
-if 10 > 100
+if 10 > 20
     print "Print me baby one more time as 10 is greater than zero!"
+  elsif 10 < 5
+    puts "ten is less than five?"
   else
     print "That means i'll get printed"
   end
