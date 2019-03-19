33f0f6d682512baa7b2db406bf28644e1ad6b9a3
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 15:18:50 +0000

    And

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,11 +1,8 @@
-# test_1 = 77 != 77
-test_1 = false
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
 
-# test_2 = -4 <= -4
-test_2 = true
+# boolean_2 = true && 100 >= 100
+boolean_2 = true
 
-# test_3 = -44 < -33
-test_3 =  true
-
-# test_4 = 100 == 1000
-test_4 = false
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
