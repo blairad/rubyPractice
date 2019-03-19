756b3710933b8ae2cb9fea2dc080ef6eb1b99e55
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 15:14:28 +0000

    practice makes perfect

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,7 +1,11 @@
-test_1 = 17 > 16
+# test_1 = 77 != 77
+test_1 = false
 
-test_2 = 21 <= 30
+# test_2 = -4 <= -4
+test_2 = true
 
-test_3 = 9 >= 9
+# test_3 = -44 < -33
+test_3 =  true
 
-test_4 = -11 < 4
+# test_4 = 100 == 1000
+test_4 = false
