53172c01935ef69846e8bfa41088292bc321f9d7
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 13:55:23 +0000

    repeat for more input

--- practiceRuby2.rb
+++ practiceRuby2.rb
@@ -1,3 +1,12 @@
 puts "What's your first name?"
 
 first_name = gets.chomp
+
+print "what's your last name?"
+last_name = gets.chomp
+
+print "What city are you from?"
+city = gets.chomp
+
+print "What state are you from?"
+state = gets.chomp.upcase
