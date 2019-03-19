e71533f79cbb46dc30102c512e37ca1362d7c2cc
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 14:14:39 +0000

    printing the output

--- practiceRuby2.rb
+++ practiceRuby2.rb
@@ -1,12 +1,15 @@
-puts "What's your first name?"
-
+print "What's your first name?"
 first_name = gets.chomp
+puts "Your name is #{first_name}!"
 
 print "what's your last name?"
 last_name = gets.chomp
+puts "Your last name is #{last_name}!"
 
 print "What city are you from?"
 city = gets.chomp
+puts "you are from #{city}!"
 
 print "What state are you from?"
 state = gets.chomp.upcase
+puts "you are from the state of #{state}!"
