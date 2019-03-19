87a00db8915ae65829aa7117f391353c08eb1e00
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-19 15:33:54 +0000

    unless

--- practiceRuby3.rb
+++ practiceRuby3.rb
@@ -1,9 +1,2 @@
-a = 20
-b = 21
-if a < b
-  print "a is less than b!"
-elsif b < a
-  print "b is less than a!"
-else
-  print "b is equal to a!"
-end
+problem = false
+print "you can't do that!" unless problem
