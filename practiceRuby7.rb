88e880143c18345f2c17994e8c2391c6fd9d1bab
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 16:30:55 +0000

    hashes

--- practiceRuby7.rb
+++ practiceRuby7.rb
@@ -1,3 +1,11 @@
-my_array = [[1,1,1,1,1], ["this", "is", "a", "string?"], [true, false]]
+my_hash = {
+  "dumpling" => 5.5,
+  "no dumpling" => 0,
+  "noodles" => 5.75
+  }
 
-#again right. i hope i'm learning
+my_hash = Hash.new
+my_hash["dog"] = "woof"
+my_hash["cat"] = "meow"
+
+# worked this time. still don't understand how it didn't last time...
