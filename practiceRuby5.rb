00661b94732ea2b251a5fc227957b6cd4579865a
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 13:22:00 +0000

    iterating with .times

--- practiceRuby5.rb
+++ practiceRuby5.rb
@@ -1,14 +1,5 @@
-i = 0
-loop do
-  print "Ruby!"
-  i += 1
-  break if i == 30
-end
+30.times { print "Ruby!" }
 
-#i had this below that wouldn't work even though it printed out 'ruby' 30 times. online courses can be frustrating
-#i = 0
-#loop do
-#  i += 1 
-#  print "ruby "
-#  break if i > 29
-#end
+30.times {print "ruby "}
+
+#top correct but bottom not and both still print out same... agh
