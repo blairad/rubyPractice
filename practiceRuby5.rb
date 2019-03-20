cde2cd7c952ab82553738528bdf9324d3a9bd64d
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 12:25:03 +0000

    next

--- practiceRuby5.rb
+++ practiceRuby5.rb
@@ -1,6 +1,11 @@
 i = 20
 loop do
-  i -= 1
-  print "#{i}"
-  break if i <= 0
+i -= 1
+next if i % 2 == 1
+print "#{i}"
+break if i <= 0
 end
+
+#had problems with this one on the site. still not sure what i was 
+# doing was as i kept breaking codecadmy. found someone else who wrote
+# same answer but theirs worked??
