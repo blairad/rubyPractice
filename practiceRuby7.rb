1c7d4b8b2ac781ee9b5da951c76588c02894ec92
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 16:01:36 +0000

    accessing hash values

--- practiceRuby7.rb
+++ practiceRuby7.rb
@@ -1,2 +1,19 @@
 pets = Hash.new
-pets["gary"] = "cat"
+
+pets["Luna"] = "cat"
+
+puts pets["Luna"]
+
+=begin 
+pets = Hash.new
+pets = {
+  "fiona" => "hamster",
+  "gary" => "cat",
+  "andrew" => "dog"
+}
+puts pets ["fiona"]
+=end 
+
+# the comment is what i put for my answer which printed what i expected 
+# to the console. it was similar to the example. but wouldn't be given
+# correct by the website. more frustration...
