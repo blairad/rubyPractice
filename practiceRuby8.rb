67aee68f78f63427aaf0682c37d524dccb0b02ae
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 17:03:43 +0000

    creating the frequencies hash

--- practiceRuby8.rb
+++ practiceRuby8.rb
@@ -1,3 +1,8 @@
 puts "something here: "
 text = gets.chomp
 words = text.split(" ")
+
+frequencies = Hash.new(0)
+
+#the (" ") on line 3 was asked for previous but it breaks the compiler on 
+#codecademy. so getting confused
