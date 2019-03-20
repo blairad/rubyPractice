29e965ef39fefbdc68f9617f4d5dcbd1ae73c175
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 16:08:38 +0000

    iterating over arrays

--- practiceRuby7.rb
+++ practiceRuby7.rb
@@ -1,19 +1,3 @@
-pets = Hash.new
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
 
-pets["Luna"] = "cat"
-
-puts pets["Luna"]
-
-=begin 
-pets = Hash.new
-pets = {
-  "fiona" => "hamster",
-  "gary" => "cat",
-  "andrew" => "dog"
-}
-puts pets ["fiona"]
-=end 
-
-# the comment is what i put for my answer which printed what i expected 
-# to the console. it was similar to the example. but wouldn't be given
-# correct by the website. more frustration...
+languages.each { |element| puts element}
