29163df6c57df1084c0829d4b9a5852592bd014d
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 15:13:58 +0000

    redacted

--- practiceRuby6.rb
+++ practiceRuby6.rb
@@ -1,4 +1,24 @@
-user = gets.chomp
+puts "Enter some text: "
+text = gets.chomp
+
+puts "Enter words to redact: "
 redact = gets.chomp
 
 words = text.split(" ")
+words.each { |word| print word }
+
+=begin
+text = gets.chomp
+
+redact = gets.chomp
+
+words = text.split(" ")
+
+words = ["these are some words here maybe?"]
+words.each do |words|
+  print words
+end
+
+=end
+
+#i don't feel the explination is very clear again for what is expected...
