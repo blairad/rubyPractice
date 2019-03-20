e4667c78ec3bc48fc4d208abdb0911eff70b2ec2
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-20 13:18:23 +0000

    loop loop loop etc

--- practiceRuby5.rb
+++ practiceRuby5.rb
@@ -1,6 +1,14 @@
-for i in 1..50
-    print i
-    i += 1
-  end
+i = 0
+loop do
+  print "Ruby!"
+  i += 1
+  break if i == 30
+end
 
-  
+#i had this below that wouldn't work even though it printed out 'ruby' 30 times. online courses can be frustrating
+#i = 0
+#loop do
+#  i += 1 
+#  print "ruby "
+#  break if i > 29
+#end
