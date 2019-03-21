6bba7afab7748fb005a44e36403f4c2ef8d2aa71
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 16:03:21 +0000

    hash rocket

--- practiceRuby11.rb
+++ practiceRuby11.rb
@@ -1,10 +1,6 @@
-strings == ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
-symbols = []
+movies ={
+  :"Point Break" => "cops and robber bromance",
+  :"Home Alone" => "He makes his parents disappear",
+  }
 
-strings.each do |s| 
- symbols.push(s.intern)
-end 
-print symbols
-
-#intern works the same as .to_sym and may be used together
-#a warning popped up to add the extra = to strings... but it still worked without
+  #small mistake forgetting the ,
