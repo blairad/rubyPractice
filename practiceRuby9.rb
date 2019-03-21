7818921af34dac816fbe126b4a3ccdf01e53ee24
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 11:09:42 +0000

    call it

--- practiceRuby9.rb
+++ practiceRuby9.rb
@@ -1,12 +1,11 @@
-# Define your method below!
-def greeting
-    puts "a greeting here"
+def array_of_10
+    puts (1..10).to_a
   end
   
-  
-  
-  
-  # Define your method above this line.
-  
-  greeting # Ignore this for now. We'll explain
-           # it in the next exercise!
+  array_of_10
+
+=begin
+You call a method just by typing its name. R
+emember when you saw us type puts_1_to_10 or greeting after our method 
+definition in the last two exercises? That was us calling our methods!
+=end
