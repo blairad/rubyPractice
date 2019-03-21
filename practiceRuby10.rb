2d8a3819395f9bdbfed274e75ed07eeae7bd1850
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 14:19:31 +0000

    sorting with control flow

--- practiceRuby10.rb
+++ practiceRuby10.rb
@@ -1,2 +1,13 @@
 def alphabetize(arr, rev=false)
-end
+    arr.sort!
+    if rev == true
+      return arr.reverse!
+    else
+      return arr
+  end
+  end
+  
+  numbers = [5, 7, 1, 4, 9, 3]
+  puts numbers
+
+  #some issues but slowly getting a little understanding
