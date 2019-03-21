a7e66a175447b9a7de16b01800ad72e98078c9db
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 11:02:33 +0000

    create your own

--- practiceRuby9.rb
+++ practiceRuby9.rb
@@ -1,5 +1,12 @@
-def puts_1_to_10
-    (1..10).each { |i| puts i }
+# Define your method below!
+def greeting
+    puts "a greeting here"
   end
   
-  puts_1_to_10 # Ignore this for now. We'll explain it soon!
+  
+  
+  
+  # Define your method above this line.
+  
+  greeting # Ignore this for now. We'll explain
+           # it in the next exercise!
