08ece65d123a37dfc10fca3118abac2d2b07b6f6
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 13:28:46 +0000

    sorting

--- practiceRuby9.rb
+++ practiceRuby9.rb
@@ -1,18 +1,6 @@
-my_array = [1, 2, 3, 4, 5]
-
-my_array.each do |num|
-  puts num * num
+fruits = ["orange", "apple", "banana", "pear", "grapes"]
+fruits.sort! do |firstFruit, secondFruit|
+  secondFruit <=> firstFruit
 end
 
-=begin
-my_array = [1, 2, 3, 4, 5]
-
-my_array.each do |num|
-  sum num * num
-end
-puts sum
-
-mine here. sorta getting there. struggled with the |num| and 
-not sure i understand it fully
-
-=end
+#hmm got this first attempt
