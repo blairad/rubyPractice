1accaeca2e3cf6c561abd0a869b92ecc66fb77cd
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 16:45:29 +0000

    more methods/solutions

--- practiceRuby11.rb
+++ practiceRuby11.rb
@@ -1,7 +1,7 @@
 movie_ratings = {
   memento: 3,
   primer: 3.5,
-  the_matrix: 5,
+  the_matrix: 3,
   truman_show: 4,
   red_dawn: 1.5,
   skyfall: 4,
@@ -9,14 +9,24 @@ movie_ratings = {
   uhf: 1,
   lion_king: 3.5
 }
+# Add your code below!
 
-good_movies = movie_ratings.select { |name, rating| rating > 3 }
+movie_ratings.each_key {|key| puts key}
 
 =begin
-movie_ratings = {good_movies: > 3 }
+We’ve often found we only want the key or value associated with a key/value pair, and it’s kind of a pain to put both into our block and only work with one. Can we iterate over just keys or just values?
 
-movie_ratings.select { |film, score| score > 3}
+This is Ruby. Of course we can.
 
-ballsed this a bit but not far wrong. adding more than i need quite often
+Ruby includes two hash methods, .each_key and .each_value, that do exactly what you’d expect:
 
+my_hash = { one: 1, two: 2, three: 3 }
+
+my_hash.each_key { |k| print k, " " }
+# ==> one two three
+
+my_hash.each_value { |v| print v, " " }
+# ==> 1 2 3
 =end
+
+#had two arguments first but realised mistake and fixed it
