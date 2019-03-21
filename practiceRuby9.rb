e39a5b2ff52803f053d225ccee5eca0b9df79b46
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 11:44:54 +0000

    let's learn return

--- practiceRuby9.rb
+++ practiceRuby9.rb
@@ -1,11 +1,3 @@
-def array_of_10
-    puts (1..10).to_a
+def add(i, n)
+    return i + n
   end
-  
-  array_of_10
-
-=begin
-You call a method just by typing its name. R
-emember when you saw us type puts_1_to_10 or greeting after our method 
-definition in the last two exercises? That was us calling our methods!
-=end
