18b5f5e47b6892758b23618a95389db07e821024
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 16:38:58 +0000

    becoming more selective

--- practiceRuby11.rb
+++ practiceRuby11.rb
@@ -1,6 +1,22 @@
-movies ={
-  PointBreak:  "cops and robber bromance",
-  HomeAlone: "He makes his parents disappear",
-  }
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 5,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
 
-  #hash rockets gone : symbol at end of hash now
+good_movies = movie_ratings.select { |name, rating| rating > 3 }
+
+=begin
+movie_ratings = {good_movies: > 3 }
+
+movie_ratings.select { |film, score| score > 3}
+
+ballsed this a bit but not far wrong. adding more than i need quite often
+
+=end
