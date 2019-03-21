7b6884fac54dd8a3f755427fa6a76c073e7f9706
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 15:31:09 +0000

    what are symbols used for

--- practiceRuby11.rb
+++ practiceRuby11.rb
@@ -1,1 +1,10 @@
-my_first_symbol = :something
+symbol_hash = {
+  :one => 1,
+  :two => 2,    # Fill in these two blanks!
+  :three => 3,
+}
+
+=begin
+Symbols pop up in a lot of places in Ruby, but they’re primarily 
+used either as hash keys or for referencing method names.
+=end
