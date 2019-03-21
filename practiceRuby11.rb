04c376066b9f36f1c8df5ee6abcf0e238b56695f
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 14:53:47 +0000

    iterating over hashes

--- practiceRuby11.rb
+++ practiceRuby11.rb
@@ -1,1 +1,12 @@
-my_hash = Hash.new
+matz = { "First name" => "Yukihiro",
+  "Last name" => "Matsumoto",
+  "Age" => 47,
+  "Nationality" => "Japanese",
+  "Nickname" => "Matz"
+}
+
+matz.each do |key, answer|
+  puts matz[key]
+end
+
+# sorta getting it 
