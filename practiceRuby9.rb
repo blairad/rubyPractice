78d20102ba5414cc1bf8cf514d01d44902e80e79
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-21 13:26:12 +0000

    blocks

--- practiceRuby9.rb
+++ practiceRuby9.rb
@@ -1,6 +1,18 @@
-def welcome(name)
-    return "hello, #{name}"
-  end
-  
-  
-  
+my_array = [1, 2, 3, 4, 5]
+
+my_array.each do |num|
+  puts num * num
+end
+
+=begin
+my_array = [1, 2, 3, 4, 5]
+
+my_array.each do |num|
+  sum num * num
+end
+puts sum
+
+mine here. sorta getting there. struggled with the |num| and 
+not sure i understand it fully
+
+=end
