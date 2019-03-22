5f524daeee33bfbeccbf9b8f74415191d5659c5c
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 12:18:34 +0000

    when and then the case statement

--- practiceRuby13.rb
+++ practiceRuby13.rb
@@ -1,2 +1,25 @@
-# Type your Ruby code below!
-puts 4 > 5 ? "4 is more than 5?" : "4 is not more than 5!"
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+
+
+case greeting
+  when "English" then puts "Hello!"
+  when "French" then puts "Bonjour!"
+  when "German" then puts "Guten Tag!"
+  when "Finnish" then puts "Haloo!"
+  else puts "I don't know that language!"
+end
+    
+=begin
+    case greeting 
+    when "English" then puts "Hello!" 
+    when "French" then puts "Bonjour!" 
+    when "German" then puts "Guten Tag!" 
+    when "Finnish" then puts "Haloo!" 
+    else puts "I don't know that language!" 
+    end
+=end
+
+#bottom is mine. doesnt compile but is identical?
