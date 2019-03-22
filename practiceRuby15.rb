4c2a905e38d9168ecce7a8a7ef0634c5b3184562
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 15:27:09 +0000

    yielding with parameters

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,20 +1,12 @@
-fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-# Add your code below!
-
-double = Proc.new {|x| x * 2}
-
-doubled_fibs = fibs.collect(&double)
-
-puts doubled_fibs
-
-=begin
-
-double_fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-double_fibs.collect { |fibs| fibs ** 2 }
-puts double_fibs
-
-don't remember some of the actual answer from prevous lessons. 
-Proc ???
-
-=end
+def yield_name(name)
+    puts "In the method! Let's yield."
+    yield("Kim")
+    puts "In between the yields!"
+    yield(name)
+    puts "Block complete! Back in the method."
+  end
+  
+  yield_name("Eric") { |n| puts "My name is #{n}." }
+  
+  # Now call the method with your name!
+  yield_name("andrew") { |n| puts "My name is #{n}." }
