5494df8770e54c17e45a4e40f76251234465fddb
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 13:17:42 +0000

    omit needless words

--- practiceRuby14.rb
+++ practiceRuby14.rb
@@ -1,23 +1,28 @@
-$VERBOSE = nil    # We'll explain this at the end of the lesson.
+require 'prime'   # This is a module. We'll cover these soon!
+
+def first_n_primes(n)
+  return "n must be an integer." unless n.is_a? Integer
+  return "n must be greater than 0." if n <= 0
+  return Prime.first n
+end
+
+first_n_primes(10)
+
+=begin
+
 require 'prime'   # This is a module. We'll cover these soon!
 
 def first_n_primes(n)
 
-  unless n.is_a? Integer
-    return "n must be an integer."
-  end
+  n.is_a? Integer unless return "n must be an integer."
+   
+  n <= 0 if return "n must be greater than 0."
+    
 
-  if n <= 0
-    return "n must be greater than 0."
-  end
-  
-  prime_array = [] if prime_array.nil?
-  
-  prime = Prime.new
-  for num in (1..n)
-    prime_array.push(prime.next)
-  end
-  return prime_array
+  return Prime.first n
 end
 
 first_n_primes(10)
+
+=end
+# think i see where wrong
