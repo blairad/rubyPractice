d6aa2106e709c4034314534471a176893132d529
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 11:06:47 +0000

    not my type

--- practiceRuby12.rb
+++ practiceRuby12.rb
@@ -6,43 +6,14 @@ movies = {
 puts "What would you like to do? "
 
     choice = gets.chomp
-    
-    case choice
-    when "add"
-      puts "What movie would you like to add? "
-      title = gets.chomp
-      puts "What rating does the movie have? "
-      rating = gets.chomp
-      movies[title.to_s] = rating
-    when "update"
-      puts "Updated!"
-    when "display"
-      puts "Movies!"
-    when "delete"
-      puts "Deleted!"
-    else
-      puts "Error!"
-    end
-
-=begin
-
-movies = {
-  JurassicPark: 4.9, 
- 	HomeAlone: 4.7,
-  FreeSolo: 4.8
-  }
-
-puts "What would you like to do? "
-
-choice = gets.chomp
-
+      
 case choice
 when "add"
-  puts "Movie title to add"
-  title = gets.chomp
-  puts "what is its rating"
-  rating = gets.chomp
-  movies[title.to_s] = rating
+  puts "What movie would you like to add? "
+  title = gets.chomp.to_sym
+  puts "What rating does the movie have? "
+  rating = gets.chomp.to_i
+  movies[title] = rating
 when "update"
   puts "Updated!"
 when "display"
@@ -53,7 +24,15 @@ else
   puts "Error!"
 end
 
-=end
+=begin
 
-#fucked if i know what is wrong as i think it is identical to 
-#the solution but getting an error...
+i fucked up and had the .to_sym in  [ ]
+
+case choice
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp[.to_sym]
+  puts "What rating does the movie have? "
+  rating = gets.chomp[.to_i]
+  movies[title.to_s] = rating
+  =end
