f50532546c2907f6335cbd415ec1faea7d32bc80
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 15:29:47 +0000

    tried it myself

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,12 +1,4 @@
-def yield_name(name)
-    puts "In the method! Let's yield."
-    yield("Kim")
-    puts "In between the yields!"
-    yield(name)
-    puts "Block complete! Back in the method."
+def double(i)
+    puts i * 2
+    yield (i * 10)
   end
-  
-  yield_name("Eric") { |n| puts "My name is #{n}." }
-  
-  # Now call the method with your name!
-  yield_name("andrew") { |n| puts "My name is #{n}." }
