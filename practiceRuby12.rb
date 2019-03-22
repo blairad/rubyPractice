38124878a51fbb231199eb5567bfbcfceddbf96e
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 10:55:31 +0000

    prompting redux

--- practiceRuby12.rb
+++ practiceRuby12.rb
@@ -1,6 +1,35 @@
 movies = {
-  StarWars: 4.8, 
-  Divergent: 4.7
+  JurassicPark: 4.9, 
+ 	HomeAlone: 4.7,
+  FreeSolo: 4.8
+  }
+puts "What would you like to do? "
+
+    choice = gets.chomp
+    
+    case choice
+    when "add"
+      puts "What movie would you like to add? "
+      title = gets.chomp
+      puts "What rating does the movie have? "
+      rating = gets.chomp
+      movies[title.to_s] = rating
+    when "update"
+      puts "Updated!"
+    when "display"
+      puts "Movies!"
+    when "delete"
+      puts "Deleted!"
+    else
+      puts "Error!"
+    end
+
+=begin
+
+movies = {
+  JurassicPark: 4.9, 
+ 	HomeAlone: 4.7,
+  FreeSolo: 4.8
   }
 
 puts "What would you like to do? "
@@ -9,7 +38,11 @@ choice = gets.chomp
 
 case choice
 when "add"
-  puts "Added!"
+  puts "Movie title to add"
+  title = gets.chomp
+  puts "what is its rating"
+  rating = gets.chomp
+  movies[title.to_s] = rating
 when "update"
   puts "Updated!"
 when "display"
@@ -20,28 +53,7 @@ else
   puts "Error!"
 end
 
-=begin 
-
-movies = {
-  JurassicPark: 10,
-  HomeAlone: 8
-  }
-puts "what is your favourite film"
-choice = gets.chomp
-
-case choice
-  when "add"
-  	puts "Added!"
-  when "update"
-  	puts "Updated!"
-  when "display"
-  	puts "Movies"
-  when "delete"
-  	puts "Deleted!"
-	else 
-  	puts "Error!"
-end
-
 =end
 
-#mine in comment is wrong but almost identical?
+#fucked if i know what is wrong as i think it is identical to 
+#the solution but getting an error...
