cbf31f5569f6fd6fa58441ea2fb9160e17eaaa43
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 12:36:20 +0000

    the right tool for the job

--- practiceRuby13.rb
+++ practiceRuby13.rb
@@ -1,5 +1,3 @@
-def multiple_of_three(n)
-    n % 3 == 0 ? "True" : "False"
- end
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
- #i want this tutorial to be over
+my_array.each { |num| puts num unless num % 2 !=0 }
