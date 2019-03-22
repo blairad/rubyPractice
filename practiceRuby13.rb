a102a5c25b2f55388853cc6a1589f48cd56f1dd0
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 12:55:15 +0000

    ternary operator

--- practiceRuby13.rb
+++ practiceRuby13.rb
@@ -1,3 +1,11 @@
-if 1 < 2 then puts "One is less than two!" end
+puts 1 < 2 ? "One is less than two!" : "One is not less than two."
 
-  
+=begin
+
+sum = 1 < 2
+  puts sum "One is less than two!" : "One is not less than two."
+
+=end
+
+#hmm close? followed the examples...
+
