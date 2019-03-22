6b9e65ad365d3134bc27454eb143642a7c3b7517
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 10:30:01 +0000

    the case statement

--- practiceRuby12.rb
+++ practiceRuby12.rb
@@ -1,5 +1,47 @@
 movies = {
-  'Jurassic Park' => 10
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "Added!"
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
+
+=begin 
+
+movies = {
+  JurassicPark: 10,
+  HomeAlone: 8
   }
 puts "what is your favourite film"
 choice = gets.chomp
+
+case choice
+  when "add"
+  	puts "Added!"
+  when "update"
+  	puts "Updated!"
+  when "display"
+  	puts "Movies"
+  when "delete"
+  	puts "Deleted!"
+	else 
+  	puts "Error!"
+end
+
+=end
+
+#mine in comment is wrong but almost identical?
