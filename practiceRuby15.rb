71f3b63cd40ff82ba37de7b1c8576e42fb2d6a27
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 15:21:55 +0000

    collect them all

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,3 +1,20 @@
-5.times do 
-  puts "I'm a block!"
-end
+fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# Add your code below!
+
+double = Proc.new {|x| x * 2}
+
+doubled_fibs = fibs.collect(&double)
+
+puts doubled_fibs
+
+=begin
+
+double_fibs = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+double_fibs.collect { |fibs| fibs ** 2 }
+puts double_fibs
+
+don't remember some of the actual answer from prevous lessons. 
+Proc ???
+
+=end
