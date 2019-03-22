5405848a4f46034e091465701a206885ece4fa3d
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 13:18:45 +0000

    less is more

--- practiceRuby14.rb
+++ practiceRuby14.rb
@@ -2,27 +2,10 @@ require 'prime'   # This is a module. We'll cover these soon!
 
 def first_n_primes(n)
   return "n must be an integer." unless n.is_a? Integer
-  return "n must be greater than 0." if n <= 0
-  return Prime.first n
+  return "n must be greater than 0." unless n > 0
+  Prime.first n
 end
 
 first_n_primes(10)
 
-=begin
-
-require 'prime'   # This is a module. We'll cover these soon!
-
-def first_n_primes(n)
-
-  n.is_a? Integer unless return "n must be an integer."
-   
-  n <= 0 if return "n must be greater than 0."
-    
-
-  return Prime.first n
-end
-
-first_n_primes(10)
-
-=end
-# think i see where wrong
+#removing more returns
