8d74282c3fd9ebdec3f9e9f14c87aeaa9dd6c07a
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-22 11:37:07 +0000

    update

--- practiceRuby12.rb
+++ practiceRuby12.rb
@@ -1,25 +1,33 @@
 movies = {
-  JurassicPark: 4.9, 
- 	HomeAlone: 4.7,
-  FreeSolo: 4.8
+  StarWars: 4.8, 
+  Divergent: 4.7
   }
+
 puts "What would you like to do? "
 
-    choice = gets.chomp
-      
+choice = gets.chomp
+
 case choice
- when "add"
-    puts "What movie would you like to add? "
-    title = gets.chomp
-    if movies[title.to_sym].nil? 
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
     puts "What rating does the movie have? "
     rating = gets.chomp
     movies[title.to_sym] = rating.to_i
-    else
+  else
     puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
-    end
+  end
 when "update"
-  puts "Updated!"
+  puts "What movie would you like to update? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "That movie does not exist."
+  else
+    puts "What is the new rating? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
 when "display"
   puts "Movies!"
 when "delete"
