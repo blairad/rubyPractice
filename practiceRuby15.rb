c471e7369c30e21b4e104b660d0fe67d5657b585
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-26 11:42:01 +0000

    proc syntax

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,4 +1,16 @@
-def double(i)
-    puts i * 2
-    yield (i * 10)
-  end
+floats = [1.2, 3.45, 0.91, 7.727, 11.42, 482.911]
+# Write your code below this line!
+
+round_down = Proc.new { |x| x.floor }
+
+# Write your code above this line!
+ints = floats.collect(&round_down)
+print ints
+
+
+=begin
+round_down = Proc.floor {|x| x ** 3  }
+
+not a good attempt
+
+=end
