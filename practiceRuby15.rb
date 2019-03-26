84d9ab3cd0fc43c32cd5fcb2dedc5e8eb83f0bc1
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-26 11:55:06 +0000

    call me maybe

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,23 +1,3 @@
-def greeter
-    yield
-  end
-  
-  phrase = Proc.new { puts "Hello there!" }
-  
-  greeter(&phrase)
+hi = Proc.new {puts "Hello!"}
 
-
-=begin
-
-def greeter
-    yeild
-  end
-  
-  phrase = Proc.new do {|greeter| }
-    puts "hello there"
-  end
-
-  =end
-
-  # i was sorta there but get lost when it says follow example but
-  #something different is needed
+hi.call
