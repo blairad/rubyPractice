487b21e062106333aca3831d7eba4d46e07d4e08
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-26 12:14:03 +0000

    a quick review

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,23 +1,16 @@
-my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+Quick Review
+All this talk of blocks, procs, and lambdas might have your head spinning. 
+Let’s take a minute to clarify exactly what each one is:
 
-# Add your code below!
-symbol_filter = lambda {|x| x.is_a? Symbol}
-symbols = my_array.select(&symbol_filter)
+A block is just a bit of code between do..end or {}. It’s not an object
+ on its own, but it can be passed to methods like .each or .select.
 
-my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+ A proc is a saved block we can use over and over.
 
-puts symbols
+A lambda is just like a proc, only it cares about the number of 
+arguments it gets and it returns to its calling method rather than 
+returning immediately.
 
-=begin
-
-my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
-
-# Add your code below!
-
-symbol_filter = (lambda {|x| x.to_sym})
-
-symbols = my_array.select (symbol_finder)
-
-puts symbols
-
-=end
+There are obviously lots of cases in which blocks, procs, and 
+lambdas can do similar work, but the exact circumstances of your 
+program will help you decide which one you want to use.
