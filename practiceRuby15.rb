c74479e9c3db26c52db997a87e3709c4b0e3edd7
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-26 11:59:56 +0000

    symbols meet procs

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -1,3 +1,16 @@
-hi = Proc.new {puts "Hello!"}
+numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-hi.call
+strings_array = numbers_array.map(&:to_s)
+
+puts strings_array
+
+
+=begin
+numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+strings_array =  numbers_array.map(&:to_i)
+
+puts strings_array
+=end
+
+#i'm an idiot
