e300b4125d1fc765ae1cc154bbbe191d8fb11b75
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 11:59:16 +0000

    class basics

--- practiceRuby16.rb
+++ practiceRuby16.rb
@@ -1,17 +1,6 @@
-class Creature
-    def initialize(name)
-      @name = name
-    end
-    
-    def fight
-      return "Punch to the chops!"
-    end
-  end
-  
-  # Add your code below!
-  class Dragon < Creature
-    def fight
-      puts "Instead of breathing fire..."
-      super 
+class Message 
+    def initialize(from, to)
+      @from = from
+      @to = to
     end
   end
