ec4c0b7d4416118db2c146e511f0423201ec9d20
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 11:32:59 +0000

    naming  variables for instance

--- practiceRuby16.rb
+++ practiceRuby16.rb
@@ -1,8 +1,16 @@
-class MyClass
-    $my_variable = "Hello!"
+class Person
+    def initialize(name, age, profession)
+      @name = name
+      @age = age
+      @profession = profession
+    end
   end
-  
-  puts $my_variable
 
-  #need to the $ on the variable to make it global and work outside the
-  # class.
+=begin
+
+A caveat, though: global variables can be changed from anywhere in 
+your program, and they are generally not a very good idea. 
+It’s much better to create variables with limited scope that can 
+only be changed from a few places!
+
+=end
