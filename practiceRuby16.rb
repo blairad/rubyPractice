a252aff7b0be2d1729be36eeb4a2c6e4420a3f85
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 12:03:23 +0000

    class variable

--- practiceRuby16.rb
+++ practiceRuby16.rb
@@ -1,6 +1,22 @@
 class Message 
+    @@messages_sent = 0
+    def initialize(from, to)
+      @from = from 
+      @to = to 
+      @@messages_sent +=1 
+    end
+  end
+  
+=begin
+  
+  class Message 
     def initialize(from, to)
       @from = from
       @to = to
+      @@messages_sent += 1
     end
   end
+  
+=end
+
+#need to define @@messages_sent = 0 before initalize
