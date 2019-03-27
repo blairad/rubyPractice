b7cce40c8932fef1bbaae6d4a62257925329b546
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 10:35:13 +0000

    passing proc to method

--- practiceRuby15.rb
+++ practiceRuby15.rb
@@ -2,12 +2,14 @@ ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
 
 # Add your code below!
 under_100 = Proc.new { |x| x < 100 }
-=begin
+youngsters = ages.select(&under_100)
+
 
 ages = [23, 101, 7, 104, 11, 94, 100, 121, 101, 70, 44]
 
 # Add your code below!
 
-under_100 = Proc.new {|x|  x.select}
 
-=end
+puts youngsters
+
+#had it the wrong way around initialy
