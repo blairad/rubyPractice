3643afa227557e1a365bb0c7b0cd9f4d3e5cb2a4
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 12:13:45 +0000

    create an instance of our Message class.

--- practiceRuby16.rb
+++ practiceRuby16.rb
@@ -7,16 +7,4 @@ class Message
     end
   end
   
-=begin
-  
-  class Message 
-    def initialize(from, to)
-      @from = from
-      @to = to
-      @@messages_sent += 1
-    end
-  end
-  
-=end
-
-#need to define @@messages_sent = 0 before initalize
+  my_message = Message.new("andrew", "doug")
