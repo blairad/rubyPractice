a641cb81f97a7dbaca1027f03b306d1e7bc50e74
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-27 11:16:11 +0000

    instance variable

--- practiceRuby16.rb
+++ practiceRuby16.rb
@@ -1,6 +1,9 @@
 class Person
-    def initialize
+    def initialize(name)
+      @name = name
     end
   end
   
-  
+  #In Ruby, we use @ before a variable to signify that it’s an 
+  #instance variable. This means that the variable is attached to 
+  #the instance of the class.
