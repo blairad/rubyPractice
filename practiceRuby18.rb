facb3a4f8135f24504fe43cf6d4a85b2ee4e859a
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-28 09:45:22 +0000

    include math

--- practiceRuby18.rb
+++ practiceRuby18.rb
@@ -1,3 +1,15 @@
-require "date"
-
-puts Date.today
+class Angle
+    include Math
+    attr_accessor :radians
+    
+    def initialize(radians)
+      @radians = radians
+    end
+    
+    def cosine
+      cos(@radians)
+    end
+  end
+  
+  acute = Angle.new(1)
+  acute.cosine
