3a48c9f00fdf7cf725106e3e485953c81298f30c
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-28 09:53:43 +0000

    module again

--- practiceRuby18.rb
+++ practiceRuby18.rb
@@ -1,13 +1,5 @@
-class Application
-    attr_accessor :status
-    def initialize; end
-    # Add your method here!
-    public
-    def print_status
-      puts "All systems go!"
-  end
-    private
-    def password
-      return 12345
-    end
+# Create your module below!
+
+module Languages
+    FAVE = "my favourite language is Ruby"
   end
