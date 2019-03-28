49c0e33ab94f7f9e44eeea5ea40728a0f586b1e3
Andrew Blair <blair.d.andrew@gmail.com>
2019-03-28 09:54:47 +0000

    mixing/including

--- practiceRuby18.rb
+++ practiceRuby18.rb
@@ -1,5 +1,14 @@
-# Create your module below!
-
 module Languages
-    FAVE = "my favourite language is Ruby"
+    FAVE = "Ruby"  # This is what you typed before, right? :D
   end
+  
+  class Master
+      include Languages
+    def initialize; end
+    def victory
+      puts FAVE
+    end
+  end
+  
+  total = Master.new
+  total.victory
